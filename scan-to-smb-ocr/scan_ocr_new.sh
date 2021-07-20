bdb0549b0c700d66511a1253d230d0efc1447aa8
pelzvieh <github@flying-snail.de>
2021-07-20 13:01:37 +0200

    Added signed timestamping
    A detached timestamp signature is generated for each OCR'ed file, proving is existence and content from the time it's been created.

--- scan-to-smb-ocr/scan_ocr_new.sh
+++ scan-to-smb-ocr/scan_ocr_new.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # (c) 2021 Andreas Feldner
 # Published under GPL 3, see accompanying documentation
-# 
+#
 # The intention of this script is that you set up a network scanner in a way that it puts
 # scanned files (multi-page PDF) to directory $in_base. Once completed, this script will
 # OCR-convert each file to $out_base, that might be mounted to a collaboration application
 # e.g. nextcloud.
-# 
+#
 in_base=/var/spool/scan
 out_base=/var/spool/scanbd/
-inotifywait --monitor -e close_write $in_base | awk '/.*CLOSE_WRITE.*/ { 
+inotifywait --monitor -e close_write $in_base | awk '/.*CLOSE_WRITE.*/ {
     system("ocrmypdf -l deu --skip-text " $1 "/" $3 " /var/spool/scanbd/" $3 " && rm " $1 "/" $3);
+    system("openssl ts -query -data /var/spool/scanbd/" $3 " -cert -sha256 -out - | curl -s -S -H '\''Content-Type: application/timestamp-query'\'' --data-binary @- http://zeitstempel.dfn.de/ -o /var/spool/scanbd/" $3 ".tsr");
 }'
 
